7b00bbfd86ab3c7099ed49f6f648d82078f9882d
livnoni <yehuda.livnoni@gmail.com>
2021-10-19 23:22:31 +0300

    new tab link

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const prefixInput = document.getElementById('prefix_input');
 const numberInput = document.getElementById('number_input');
 const startWithInputInput = document.getElementById('start_with_input');
 const messageTextArea = document.getElementById('message_text_area');
-const refLink = document.getElementById('link');
 
 nameInput.value = DEFAULT_VALUES.name;
 prefixInput.value = DEFAULT_VALUES.prefix;
@@ -19,15 +18,6 @@ numberInput.value = DEFAULT_VALUES.number;
 messageTextArea.value = DEFAULT_VALUES.messageTextArea;
 startWithInputInput.value = DEFAULT_VALUES.startWithInputInput;
 
-const isMobile = ()=>{
-    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-        // true for mobile device
-        return true;
-    }else{
-        // false for not mobile device
-        return false
-    }
-}
 
 const generate = ()=>{
     if(!prefixInput.value){
@@ -44,17 +34,6 @@ const generate = ()=>{
     fullMsg += ',';
     fullMsg += '\n';
     fullMsg += messageTextArea.value;
-    const desktopLink = `https://api.whatsapp.com/send?text=${encodeURIComponent(fullMsg)}&phone=${prefixInput.value}${numberInput.value}`;
-    const mobileLink = `whatsapp://send?text=${encodeURIComponent(fullMsg)}&phone=${prefixInput.value}${numberInput.value}`;
-    console.log({desktopLink, mobileLink});
-
-    const mobile = isMobile();
-    console.log('mobile is: ',mobile)
-    refLink.href = mobile ? mobileLink : desktopLink;
-    refLink.innerHTML= 'WhatsApp Link'
-
-    //some test:
-    window.location.href = mobile ? mobileLink : desktopLink;
-
-
+    const link = `https://api.whatsapp.com/send?text=${encodeURIComponent(fullMsg)}&phone=${prefixInput.value}${numberInput.value}`;
+    window.open(link, '_blank').focus();
 }
